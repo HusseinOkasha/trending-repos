2e7495ae6b7a409fd73eac3afbbcb777ecdfc815
HusseinOkasha <husseinokasha13@gmail.com>
2021-08-29 19:02:11 +0200

    Adding trending_repos app

---
+++ trending_repos/main_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+import datetime
+import requests, json
+    
+# Create your views here.
+def index(request):
+    date = datetime.datetime.now().date()
+    url = f'''https://api.github.com/search/repositories?q=created&:%3E{date}&sort=stars&order=desc&per_page=100'''
+    res = requests.get(url).json()
+    repos = res["items"] # list of 100 repos each repo has a dict.
+    lang_repo={} # language ---> repos using it
+    for repo in repos:
+        language = repo["language"]
+        if language in lang_repo:
+            lang_repo[language].append(repo["name"])
+        else:
+            lang_repo[language]= [repo["name"]]
+    context = {"lang_repo":lang_repo}
+    return render(request,'main_app/index.html',context)
